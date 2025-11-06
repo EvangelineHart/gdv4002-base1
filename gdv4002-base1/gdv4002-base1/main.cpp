ca3aab7cedce18950d6dc515273b2485087a0f53
Evangeline Jane May Hart <st20321372@outlook.cardiffmet.ac.uk>
2025-11-06 15:44:01 +0000

    added user imput

--- gdv4002-base1/gdv4002-base1/main.cpp
+++ gdv4002-base1/gdv4002-base1/main.cpp
@@ -1,7 +1,11 @@
 #include "Engine.h"
+#include "Keys.h"
+#include <bitset>
 
 // Function prototypes
 void myUpdateScene(GLFWwindow* window, double tDelta);
+void myKeyboardHandler(GLFWwindow* window, int key, int scancode, int action, int mods);
+std::bitset<5> keys{ 0x0 };
 
 int main(void) {
 
@@ -20,8 +24,6 @@ int main(void) {
 	
 	addObject("player", glm::vec2(0, 0), glm::radians(0.0f), glm::vec2(0.5f, 0.5f), "Resources\\Textures\\1000012029.png");
 	
-	setUpdateFunction(myUpdateScene);
-	
 	addObject("asteriod_1", glm::vec2(1.3f, 1.3f), 0.0f, glm::vec2(0.6f, 0.6f), "Resources\\Textures\\1000012028.png");
 	
 	addObject("asteriod_2", glm::vec2(-1.3f, -1.0f), 0.0f, glm::vec2(0.9f, 0.9f), "Resources\\Textures\\1000012028.png");
@@ -29,9 +31,14 @@ int main(void) {
 	addObject("asteriod_3", glm::vec2(1.7f, -1.5f), 0.0f, glm::vec2(1.0f, 1.0f), "Resources\\Textures\\1000012027.png");
 	
 	addObject("asteriod_4", glm::vec2(-1.8f, 1.4f), 0.0f, glm::vec2(1.0f, 0.9f), "Resources\\Textures\\1000012027.png");
+
+	
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glBlendFunc(GL_EQUAL, 0);
 	
+	setUpdateFunction(myUpdateScene);
+	setKeyboardHandler(myKeyboardHandler);
 
 	// Enter main loop - this handles update and render calls
 	engineMainLoop();
@@ -44,10 +51,80 @@ int main(void) {
 }
 void myUpdateScene(GLFWwindow* window, double tDelta) {
 	// add update code here
-	const float pi = 3.141593f;
-	const float thetaVelocity = (pi / 180.0f) * -90.0f; // 90 degrees stored as radians
-	GameObject2D* player1 = getObject("player");
-	player1->orientation = player1->orientation + (thetaVelocity * tDelta);
+	
+	static float playerSpeed = 1.0f; // distance per second
+	static float rotationSpeed = 1.0f;
+
+	GameObject2D* player = getObject("player");
+
+	if (keys.test(Key::W) == true) {
+
+		player->position.y += playerSpeed * (float)tDelta;
+	}
+	if (keys.test(Key::S) == true) {
+
+		player->position.y -= playerSpeed * (float)tDelta;
+	}
+	if (keys.test(Key::A) == true) {
+		
+		player->orientation += 0.03f;
+		
+	}
+	if (keys.test(Key::D) == true) {
+
+		player->orientation += -0.03f;
+	}
+
+
+}
+void myKeyboardHandler(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	// Check if the key was just pressed
+	if (action == GLFW_PRESS) {
+
+		// now check which key was pressed...
+		switch (key)
+		{
+		case GLFW_KEY_ESCAPE:
+			// If escape is pressed tell GLFW we want to close the 
+			glfwSetWindowShouldClose(window, true);
+			break;
+		case GLFW_KEY_W:
+			keys[Key::W] = true;
+			break;
+		case GLFW_KEY_S:
+			keys[Key::S] = true;
+			break;
+		case GLFW_KEY_A:
+			keys[Key::A] = true;
+			break;
+		case GLFW_KEY_D:
+			keys[Key::D] = true;
+			break;
+
+		}
+	}
+	// If not pressed, check the key has just been released
+	else if (action == GLFW_RELEASE) {
+		switch (key)
+		{
+		case GLFW_KEY_W:
+			keys[Key::W] = false;
+			break;
+		case GLFW_KEY_S:
+			keys[Key::S] = false;
+			break;
+		case GLFW_KEY_A:
+			keys[Key::A] = false;
+			break;
+		case GLFW_KEY_D:
+			keys[Key::D] = false;
+			break;
+
+		}
+
+		// handle key release events
+	}
 }
 
 
